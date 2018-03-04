671ea4a1e95075e653ff5fb2e71da114de17adc0
mercedesbenz00 <sony010521@gmail.com>
2018-03-04 21:44:27 +0100

    First Commit

---
+++ ProductBox.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.19;
+import "./SafeMath32.sol";
+
+contract ProductBox {
+    using SafeMath for uint32;
+
+    struct Product {
+        string name;
+        string description;
+        uint price;
+        uint32 quantity;
+    }
+
+    Product[] public products;
+    mapping(uint => address) public productToSeller;
+    mapping(address => uint32) public sellerProductCounter;
+
+    modifier onlyProductOwner(uint _productId){
+        require(productToSeller[_productId] == msg.sender);
+        _;
+    }
+
+    function addProduct(string _n, string _d, uint _p, uint _q) public {
+        uint _id = products.push(Product(_n, _d, _p, uint32(_q)));
+        productToSeller[_id-1] = msg.sender;
+        sellerProductCounter[msg.sender] = sellerProductCounter[msg.sender].add(1);
+    }
+
+    function removeProduct(uint _productId) public onlyProductOwner(_productId) {
+        products[_productId] = products[products.length - 1];
+        productToSeller[_productId] = productToSeller[products.length - 1];
+        products.length = products.length - 1;
+        sellerProductCounter[msg.sender] = sellerProductCounter[msg.sender].sub(1);
+    }
+
+    function sellProduct(uint _productId, uint _quantity) internal {
+        uint32 diff = uint32(products[_productId].quantity - _quantity);
+        require(diff >= 0);
+        if (diff == 0) {
+            products[_productId] = products[products.length - 1];
+            productToSeller[_productId] = productToSeller[products.length - 1];
+            products.length = products.length - 1;
+        } else {
+            products[_productId].quantity = diff;
+        }
+    }
+}
---
+++ SafeMath32.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.18;
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint32 a, uint32 b) internal pure returns (uint32) {
+    if (a == 0) {
+      return 0;
+    }
+    uint32 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint32 a, uint32 b) internal pure returns (uint32) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint32 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint32 a, uint32 b) internal pure returns (uint32) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint32 a, uint32 b) internal pure returns (uint32) {
+    uint32 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
